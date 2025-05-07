0cd5c0067ebc07f63de34690f7e38559bce762a5
Rio <yp.wang@jingda.techgit config --global uaer.name riogit config --global user.email yp.wang@jingda.tech>
2025-05-07 17:45:10 +0800

    project init

---
+++ Core/Src/gpio.c
@@ -0,0 +1,81 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    gpio.c
+  * @brief   This file provides code for the configuration
+  *          of all used GPIO pins.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "gpio.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/*----------------------------------------------------------------------------*/
+/* Configure GPIO                                                             */
+/*----------------------------------------------------------------------------*/
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
+/** Configure pins as
+        * Analog
+        * Input
+        * Output
+        * EVENT_OUT
+        * EXTI
+*/
+void MX_GPIO_Init(void)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, OLED_D0_Pin|OELD_D1_Pin|OLED_RES_Pin, GPIO_PIN_SET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, OLED_DC_Pin|OLED_CS_Pin|LED_Pin|BEEP_Pin, GPIO_PIN_SET);
+
+  /*Configure GPIO pins : OLED_D0_Pin OELD_D1_Pin OLED_RES_Pin */
+  GPIO_InitStruct.Pin = OLED_D0_Pin|OELD_D1_Pin|OLED_RES_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : OLED_DC_Pin OLED_CS_Pin LED_Pin BEEP_Pin */
+  GPIO_InitStruct.Pin = OLED_DC_Pin|OLED_CS_Pin|LED_Pin|BEEP_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 2 */
+void beep_led_control(bool open)
+{
+	HAL_GPIO_WritePin(GPIOA, BEEP_Pin, open?GPIO_PIN_RESET:GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOA, LED_Pin, open?GPIO_PIN_RESET:GPIO_PIN_SET);
+}
+
+/* USER CODE END 2 */
